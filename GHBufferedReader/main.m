031598aa390f98885917eac2f88ab0e7accb39cc
Greg Haines <gresrun@gmail.com>
2011-11-26 19:11:34 -0500

    Initial Commit

---
+++ GHBufferedReader/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  GHBufferedReader
+//
+//  Created by Greg Haines on 11/26/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
